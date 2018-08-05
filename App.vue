9d71676e7d55aabe9d53bc38a8dd90e634b633fb
bikram <ghurabikram@gmail.com>
2018-08-05 02:05:56 -0700

    Project started

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+    <root> 
+    <inputUser></inputUser>
+    </root>
+</template>
+ 
+
+<script>
+import inputUser from './components/inputUser'
+import Vue from "vue-native-core";
+import { Root } from "native-base";
+
+export default {
+   components: {
+    inputUser,
+    Root
+  },
+
+  data: {
+
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ components/inputUser.vue
@@ -0,0 +1,71 @@
+<template>
+    <nb-container>
+    <nb-header />
+      <nb-content>
+        <nb-form>
+          <nb-item>
+            <nb-input placeholder="Enter Your Name" v-model="userName"/>
+          </nb-item>
+          <nb-item>
+            <nb-text v-if="userName">Hello, {{userName}}</nb-text>
+          </nb-item>
+          <nb-item>
+            <nb-input v-model="weight" placeholder="Enter Your Weight in lb"/>
+          </nb-item>
+          <nb-item>
+            <nb-input v-model="heightFtIn" placeholder="Enter Your Height in ft"/>
+          </nb-item>
+          
+        </nb-form>
+        <nb-button transparent info :onPress="calcBmi">
+            <nb-text>Calculate BMI</nb-text>
+        </nb-button>
+        <nb-button transparent info :onPress="clearInput">
+            <nb-text>Clear</nb-text>
+        </nb-button>
+        <nb-text v-if="bmi">BMI, {{bmi}}</nb-text>
+      </nb-content>
+    </nb-container>
+    
+</template>
+
+<script>
+import { VueNativeBase } from "native-base";
+import Vue from "vue-native-core";
+
+Vue.use(VueNativeBase);
+
+export default {
+  
+  mounted: {
+    
+  },
+
+  data: {
+    userName: '',
+    weightKg: null,
+    heightFtIn: null,
+    bmi: null
+  },
+
+  methods: {
+    calcBmi() {
+      if(this.weight && this.heightFtIn) {
+        this.bmi = (this.weight * 4.88)/(this.heightFtIn*this.heightFtIn);
+      }
+    },
+    
+    clearInput() {
+      this.weight = null;
+      this.userName = '';
+      this.heightFtIn = null;
+      this.bmi = null;
+    }
+  
+  }
+};
+</script>
+
+<style>
+
+</style>
